dfe79b1bba2030a60ead976c6fff8516e0362580
allankildare <allankildaredev@gmail.com>
2024-05-12 17:21:17 -0300

    chore: adding main css style

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ['~/assets/styles/vello.css'],
   devtools: { enabled: true },
   modules: ['@pinia/nuxt', "@nuxt/ui"],
   colorMode: {
